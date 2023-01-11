d0ef72c087c5a0389ba0ec52914defe9041437b1
Paul Shertzinger <paulshertzinger@Pauls-Air.lan>
2023-01-11 13:39:56 -0500

    setup

---
+++ insurance.py
@@ -0,0 +1,17 @@
+age = 28
+
+sex = 0
+
+bmi = 26.2
+
+num_of_children = 3
+
+smoker = 0
+
+insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker -12500
+
+print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
+
+
+
+
